b92edbcda0952258b8e8a4c2e9ed45b87eb43ba6
Nsovo-web <60253275+Nsovo-web@users.noreply.github.com>
2021-04-26 09:29:30 +0200

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowHeight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowHeight - revealpoint) {
+
+            reveals[i].classList.add('active');
+            
+        } else {
+            reveals[i].classList.remove('active');
+            
+        }
+
+
+    }
+
+}
