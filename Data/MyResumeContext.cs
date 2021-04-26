2096066bb26381ed4d03045884b22e606ca63709
Nsovo-web <60253275+Nsovo-web@users.noreply.github.com>
2021-04-26 09:33:31 +0200

    Add files via upload

---
+++ Data/MyResumeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyResume.Models;
+
+namespace MyResume.Data
+{
+    public class MyResumeContext : DbContext
+    {
+        public MyResumeContext (DbContextOptions<MyResumeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyResume.Models.Movie> Movie { get; set; }
+    }
+}
